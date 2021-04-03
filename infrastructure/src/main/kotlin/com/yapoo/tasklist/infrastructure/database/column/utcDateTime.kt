8535d6113f92a7a586d535b26e8ebbaab12fce70
Kazufumi Okuda <kazufumi.okuda@justincase-tech.com>
2021-04-03 14:11:55 +0900

    feat: fix column defenition

--- infrastructure/src/main/kotlin/com/yapoo/tasklist/infrastructure/database/column/utcDateTime.kt
+++ infrastructure/src/main/kotlin/com/yapoo/tasklist/infrastructure/database/column/utcDateTime.kt
@@ -4,6 +4,8 @@ import com.yapoo.tasklist.infrastructure.valueobject.datetime.UtcDateTime
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ColumnType
 import org.jetbrains.exposed.sql.Table
+import java.sql.Timestamp
+import java.time.ZoneOffset
 
 fun Table.utcDateTime(name: String): Column<UtcDateTime> =
     registerColumn(name, UtcDateTimeColumnType())
@@ -14,7 +16,7 @@ class UtcDateTimeColumnType : ColumnType() {
         "TIMESTAMP WITH TIME ZONE"
 
     override fun valueFromDB(value: Any): Any = when (value) {
-        is UtcDateTime -> value
+        is Timestamp -> value.toInstant().atZone(ZoneOffset.UTC).let(::UtcDateTime)
         else -> error("Unexpected Time Value: $value of ${value::class.qualifiedName}")
     }
 
